7bf4a5187f336d57b8f40b7be2eac5dc99cbc79a
Lucas Schmidt <82345646+lucasschmidt015@users.noreply.github.com>
2022-05-16 20:30:51 -0300

    Numero

--- Main.java
+++ Main.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args)
     {
-        Numero N = new Numero();
+        Scanner s = new Scanner(System.in);  
+
+        Numero num = new Numero();
+        int i = 0;
+        while(i<19){
+            System.out.println("Digite um numero: ");
+            num.list.add(s.nextInt());
+            
+            i++;
+        }
+        
+        num.calculaMenorEMaior();
+        
     }
 
 }
--- Numero.java
+++ Numero.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Numero{
-    public int Numero;
+    public ArrayList<Integer> list = new ArrayList<Integer>();
+    public int maior, menor;
     
+    
+    
+    public void calculaMenorEMaior(){
+        
+        
+
+        Integer min = Collections.min(list);
+        Integer max = Collections.max(list);
+        this.maior = max;
+        this.menor = min;
+
+        System.out.printf("Menor: %d\nMaior: %d/n", this.menor, this.maior);
+    }
+
 }
