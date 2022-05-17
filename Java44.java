de1a6eb9647d1a8325bb7fadb88871a193a157c8
Lucas Schmidt <82345646+lucasschmidt015@users.noreply.github.com>
2022-05-16 21:46:30 -0300

    new version

--- Java44.java
+++ Java44.java
@@ -7,13 +7,15 @@ public class Java44 {
         Scanner ent = new Scanner(System.in);
         double num = 0, maior = 0, menor = 999999, media = 0, soma = 0, primo = 0;
         int contMaior = 0, contMenor = 0, i = 0;
-    
+        
         System.out.println("Digite o numero de registros: ");
         int NumeroRegistros = ent.nextInt();
+        double[] array = new double[NumeroRegistros];
         for(int g = 0; g < NumeroRegistros; g++)
         {
             System.out.println("Digite um número: ");
             num = ent.nextDouble();
+            array[g] = num;
             
             // armazena os nº digitados
             soma = soma + num;
@@ -39,7 +41,15 @@ public class Java44 {
         
     } // fim for
         
-        System.out.println("O maior nº digitado foi " + maior);
+        int k = 0;
+
+        while(k<NumeroRegistros)
+        {
+            System.out.print(array[k]+ ",");
+            k++;
+        }
+
+        System.out.println("\nO maior nº digitado foi " + maior);
         System.out.println("O menor nº digitado foi " + menor);
         System.out.println("A média foi " + (media = soma / NumeroRegistros));
         System.out.println("Quantidade de primos " + primo);
